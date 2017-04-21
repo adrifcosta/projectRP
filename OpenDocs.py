13eef10e61ad39059ee6b45d6ca90fa873eaa6ae
Adriana <adrianaifcosta.95@gmail.com>
2017-04-21 04:17:15 +0100

    algum código

--- OpenDocs.py
+++ OpenDocs.py
@@ -4,31 +4,133 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import decomposition
 import pandas as pd
-#dataset = np.loadtxt('X_train.txt')
-#linhas são samples
-#colunas são features
-#labels=np.loadtxt('y_train.txt')
+from sklearn import preprocessing
+import scipy.stats
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.ensemble import ExtraTreesClassifier
 
-
+features_names=pd.read_csv('features.txt', delim_whitespace=True,header=None);
+labels_multi=pd.read_csv('y_train.txt', header=None);
 dataset=pd.read_csv('X_train.txt',delim_whitespace=True,  header=None);
 dataset=dataset.as_matrix();
-print(dataset)
-print("Colunas", len(dataset[0,:]))
-print("Linhas", len(dataset[:,0]))
-#Matriz de correlação
-matrix_corr=pd.DataFrame.corr(dataset);
-plt.matshow(matrix_corr);
 
-#Redução de dimensão-PCA
+labels_multi=labels_multi.as_matrix();
+labels_bin=np.zeros(len(labels_multi));
+for i in range(0, len(labels_multi)):
+    if labels_multi[i]<4:
+        labels_bin[i]=1;
+    else:
+        labels_bin[i] = 0;
+
+#LABELS_MULTI
+#1-Walking
+#2-Walking-upstairs
+#3-Walking- downstairs
+#4-Sitting
+#5-Standing
+#6-Laying
+
+#LABELS_BIN
+#0-Not Walking
+#1-Walking
+
+#Nomes de todas as features
+features_names=features_names.as_matrix()[:,1];
+
+
+#Normalização dos dados
+#dataset_scaled tem média nula e desvio padrão unitário
+dataset_scaled=preprocessing.scale(dataset);
+
+#SELEÇAO DE FEATURES
+#-----------------------------------------------------------------
+#Matriz de correlação
+#corrcoef() usa estrutura de dados em que as features estão por linhas
+matrix_corr=np.corrcoef(dataset_scaled.transpose());
+mean_corr=labels_bin=np.zeros(len(matrix_corr[:,0]));
+
+for i in range (0, len(matrix_corr[:,0])):
+    mean_corr[i]=np.mean(matrix_corr[i,:]);
+mean_corr=np.absolute(mean_corr);
+
+
+mean_corr_cm=[];
+features_names_cm=[];
+all_indexs=[];
+
+for j in range (0, len(mean_corr)):
+    if (mean_corr[j]<0.03):
+        mean_corr_cm.append(mean_corr[j])
+        features_names_cm.append(features_names[j])
+        all_indexs.append(j)
+
+reduced_data_cm = np.zeros((len(dataset_scaled[:,1]),len(mean_corr_cm)));
+i = 0
+for k in all_indexs:
+    reduced_data_cm[:,i]=dataset_scaled[:,k]
+    i=i+1
+#reduced_data_cm dados que resultaram da redução por interpretação dos coeficientes de correlação
+#-----------------------------------------------------------------
+#Kruskall Wallis
+'''
+scores_kw_multi=[];
+pValue_kw_multi=[];
+
+scores_kw_bin=[];
+pValue_kw_bin=[];
+
+for i in range(0,(len(dataset_scaled[1,:]))):
+    data=dataset_scaled[:,i]
+    labels_bin = np.squeeze(labels_bin)
+    labels_multi=np.squeeze(labels_multi)
+    hstat,pval = scipy.stats.mstats.kruskalwallis(data,labels_bin)
+    #hstat1,pval1 = scipy.stats.mstats.kruskalwallis(data,labels_multi)
+    scores_kw_multi.append(hstat)
+    pValue_kw_multi.append(pval)
+    #scores_kw_bin.append(hstat1)
+    #pValue_kw_bin.append(pval1)
+
+print(pValue_kw_multi)
+#
+#
+# print(pValue_kw_bin)
+'''
+#-----------------------------------------------------------------
+#Recursive Feature Elimination
+'''
+model = LogisticRegression()
+rfe = RFE(model, 3)
+labels_multi=np.squeeze(labels_multi)
+rfe = rfe.fit(dataset_scaled, labels_multi)
+print(rfe.support_)
+print(rfe.ranking_)
+'''
+#-----------------------------------------------------------------
+#Feature Importance using Extra Trees Classifier
+'''
+labels_multi=np.squeeze(labels_multi)
+model = ExtraTreesClassifier()
+model.fit(dataset_scaled, labels_multi)
+print(model.feature_importances_)
+'''
+#REDUÇÃO DE DIMENSÃO
+#-----------------------------------------------------------------
+#PCA
+#fazer transposto
+
 pca=decomposition.PCA(n_components=3)
 pca.fit(dataset)
-datasetPCA=pca.transform(dataset)
+datasetPCA=pca.transform(dataset_scaled)
+print(len(datasetPCA[1,:]))
+print(len(datasetPCA[:,1]))
 
 #Scatter dos pontos obtidos pelo PCA
-colors = ['red','green','blue','purple','yellow','pink']
-fig = plt.figure(1, figsize=(4, 3))
-plt.clf()
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-ax.scatter(datasetPCA[:, 0], datasetPCA[:, 1], datasetPCA[:, 2], c=labels, cmap=matplotlib.colors.ListedColormap(colors))
-plt.show()
+ colors = ['red','green','blue','purple','yellow','pink']
+ fig = plt.figure(1, figsize=(4, 3))
+ plt.clf()
+ ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+ ax.scatter(datasetPCA[:, 0], datasetPCA[:, 1], datasetPCA[:, 2], c=labels_multi, cmap=matplotlib.colors.ListedColormap(colors))
+ plt.show()
 
